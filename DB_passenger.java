0605178151e18c472d591b9e908c39270c3e09bb
Zahra <mirzakizahra@gmail.com>
2021-10-05 15:32:25 -0700

    Add peroperties and method class db_passengers

---
+++ DB_passenger.java
@@ -0,0 +1,77 @@
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DB_passenger extends Database{
+
+    public DB_passenger() throws SQLException, ClassNotFoundException {
+    }
+    public void add_passenger(Passengers passengers) throws SQLException {
+        if (connection != null) {
+
+            Statement statement = connection.createStatement();
+            int indicator=this.check_exist_of_user(passengers);
+            if(indicator==0){
+               String sql="select id from driver where username = '"+passengers.getDriver().getUsername() +"'";
+               ResultSet resultSet=statement.executeQuery(sql);
+
+                int id_driver=0;
+                while (resultSet.next()) {
+                    id_driver=resultSet.getInt(1);
+                }
+                sql ="insert into passenger (name,age,address,username,userpassword,balance) values " +
+                        "('"+passengers.getName()+"','"+passengers.getAge()+"','"+passengers.getAddress()+
+                        "','"+passengers.getUsername()+"','"+passengers.getPassword()+"','"+passengers.getBalance()+"')";
+                int updated_field = statement.executeUpdate(sql);
+                System.out.println("user"+" "+passengers.getUsername()+" "+"Added");
+            }else {
+                System.out.println("user exist");
+            }
+
+        } else {
+            System.out.println("ERORR in Connection with DB");
+        }
+    }
+    public int check_exist_of_user(Passengers passengers) throws SQLException {
+        Statement statement = connection.createStatement();
+        int indicator=0;
+        String sql="select username from passenger where username= '"+passengers.getUsername()+"'";
+        ResultSet resultSet=statement.executeQuery(sql);
+        while (resultSet.next()){
+            indicator++;
+        }
+        return indicator;
+    }
+    public void show_passenger() throws SQLException {
+        if (connection != null) {
+            Statement statement = connection.createStatement();
+            String sql= "select * from passenger ";
+            ResultSet resultSet=statement.executeQuery(sql);
+            while (resultSet.next()){
+                System.out.println(resultSet.getString("name")+" "+
+                       resultSet.getInt("age")+" "+
+                        resultSet.getString("address")+" "+
+                        resultSet.getInt("username")+" "+
+                                resultSet.getInt("userpassword")+" "+
+                                resultSet.getInt("balance"));
+            }
+
+        }
+    }
+    public void increase_balance(int username,int fund) throws SQLException {
+        if (connection!= null){
+            Statement statement=connection.createStatement();
+            String sql="select balance from passenger where username='"+username+"'";
+            ResultSet resultSet=statement.executeQuery(sql);
+            int temp=0;
+            while (resultSet.next()){
+               temp= resultSet.getInt("balance");
+            }
+            temp=temp+fund;
+            sql="update passenger set balance= '"+temp+"' where username = '"+username+"'";
+            int number=statement.executeUpdate(sql);
+
+
+        }
+    }
+}
