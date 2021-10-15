c0d6db2dc219b3f09bf0f543c6d74cd094752759
Zahra <mirzakizahra@gmail.com>
2021-10-15 16:22:46 -0700

    Add findtripDestinationLength and width method and changeStatue method to DB_trip Class

--- DB_trip.java
+++ DB_trip.java
@@ -33,5 +33,70 @@ public class DB_trip extends Database {
             System.out.println("ERORR in Connection with DB");
         }
     }
-	
+	 public int findTripDestinationLength(int username) throws SQLException {
+        if (connection != null) {
+
+            Statement statement = connection.createStatement();
+            String sql = "select id from driver where username = '" + username + "'";
+            ResultSet resultSet = statement.executeQuery(sql);
+            int idDriver = 0;
+            while (resultSet.next()) {
+                idDriver = resultSet.getInt(1);
+            }
+            sql = "select destinationLength from trip where driverId = '" + idDriver + "' and " +
+                    "tripStatue = '"+"on"+"'";
+            resultSet = statement.executeQuery(sql);
+            int destinationLength=0;
+            while (resultSet.next()) {
+              destinationLength = resultSet.getInt(1);
+            }
+            return destinationLength;
+
+        }else {
+            System.out.println("ERORR in Connection with DB");
+        }
+        return 0;
+    }
+    public int findTripDestinationWidth(int username) throws SQLException {
+        if (connection != null) {
+
+            Statement statement = connection.createStatement();
+            String sql = "select id from driver where username = '" + username + "'";
+            ResultSet resultSet = statement.executeQuery(sql);
+            int idDriver = 0;
+            while (resultSet.next()) {
+                idDriver = resultSet.getInt(1);
+            }
+            sql = "select destinationWidth from trip where driverId = '" + idDriver + "' and " +
+                    "tripStatue = '"+"on"+"'";
+            resultSet = statement.executeQuery(sql);
+            int destinationWidth=0;
+            while (resultSet.next()) {
+                destinationWidth = resultSet.getInt(1);
+            }
+            return destinationWidth;
+
+        }else {
+            System.out.println("ERORR in Connection with DB");
+        }
+        return 0;
+    }
+    public void changeStatue(int username) throws SQLException {
+        if (connection != null) {
+
+            Statement statement = connection.createStatement();
+            String sql = "select id from driver where username = '" + username + "'";
+            ResultSet resultSet = statement.executeQuery(sql);
+            int idDriver = 0;
+            while (resultSet.next()) {
+                idDriver = resultSet.getInt(1);
+            }
+            sql = "update trip set tripStatue = 'off' where driverId = '" + idDriver + "' and " +
+                    "tripStatue = '"+"on"+"'";
+           statement.executeUpdate(sql);
+        }else {
+            System.out.println("ERORR in Connection with DB");
+        }
+
+    }
 }
