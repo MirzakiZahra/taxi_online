d2e75eea443f51835d5aa9f5021926b6aaffe23a
Zahra <mirzakizahra@gmail.com>
2021-10-15 16:15:58 -0700

    Add DB_trip Class

---
+++ DB_trip.java
@@ -0,0 +1,10 @@
+mport java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DB_trip extends Database {
+
+    public DB_trip() throws SQLException, ClassNotFoundException {
+    }
+	
+}
